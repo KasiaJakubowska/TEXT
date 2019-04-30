22672ec7d0b28d707ae4446668012db355e5eb80
KasiaJakubowska <katarzyna.jakubowska1998@gmail.com>
2019-04-30 21:06:21 +0200

    new files

---
+++ .history/main_20190426124124.js
@@ -0,0 +1,37 @@
+
+const spnText = document.querySelector('.text');
+const spnCursor = document.querySelector('.cursor');
+const txt = ['Dzień dobry!', 'Dobrze, że jesteś:)', 'Zaczynamy...']
+
+let activeLetter = -15;
+let activeText = 0;
+// Implementacja
+const addLetter = () => {
+ // Użyj w środku setTimeout
+ if(activeLetter >= 0) {
+ spnText.textContent += txt[activeText][activeLetter];}
+ activeLetter++;
+ if(activeLetter === txt[activeText].length) {
+
+     activeText++;
+     if(activeText === txt.length) return;
+
+     return setTimeout(() => {
+        activeLetter = -15;
+        spnText.textContent = '';
+        addLetter();
+     }, 2000)
+     
+ }
+ setTimeout(addLetter, 100)
+}
+
+
+addLetter(); //pierwsze wywołanie
+
+
+// Animacja kursora (zostaw bez zmian)
+const cursorAnimation = () => {
+ spnCursor.classList.toggle('active');
+}
+setInterval(cursorAnimation, 400);
